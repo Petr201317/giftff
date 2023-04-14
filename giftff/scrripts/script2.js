b941baf331de500d5da1b3b3219b048e264ea272
PetrDR <114173914+Petr201317@users.noreply.github.com>
2023-04-14 18:43:11 +0300

    initial

---
+++ giftff/scrripts/script2.js
@@ -0,0 +1,16 @@
+document.addEventListener("click", e=> {
+    if(e.target.id == 3) {
+        document.getElementsByClassName("answers")[0].style.display = "none";
+        document.getElementsByClassName("answers")[1].style.display = "none";
+        document.getElementsByClassName("answers")[2].style.display = "none";
+        document.getElementsByClassName("questionText")[0].style.display = "none";
+        document.getElementsByClassName("win_letter")[0].style.display = "block";
+        document.getElementsByClassName("where")[0].style.display = "block";
+}
+    else if (e.target.id == 2) {
+        document.getElementById(2).style.backgroundColor = "red";
+    }
+    else if (e.target.id == 1) {
+        document.getElementById(1).style.backgroundColor = "red";
+    }
+})
